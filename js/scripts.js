0e1812aa5d9f779f50a16c3f3431770c2f61de87
Student <example@example.com>
2019-10-24 14:35:36 -0700

    add  pictures, if/else branching for text/image show/hide

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,23 @@
 
 
 $(document).ready(function() {
-  // var tahr = $("#tahr");
-  // var panda = $("#panda");
-  // var leopard = $("#leopard");
+  $("form#animals").submit(function(event) {
+    event.preventDefault();
+    var panda = $("input:radio[name=animal]:checked").val();
+    var tahr = $("input:radio[name=animal]:checked").val();
+    var leopard = $("input:radio[name=animal]:checked").val();
+    console.log(panda, tahr, leopard);
 
-  if ("#tahr").click(function() {
-    var tahr = $("#tahr");
-    $("#tahr-text").show();
-  });
+    if (panda === "panda") {
+      $(".panda-info").show();
+      $(".tahr-info, .leopard-info").hide();
+    } else if (tahr === "tahr") {
+    $(".tahr-info").show();
+      $(".panda-info, .leopard-info").hide();
+    } else  {
+    $(".leopard-info").show();
+    $(".panda-info, .tahr-info").hide();
+    }
+
+});
 });
