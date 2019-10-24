9123138a063dba44df1d8fa5cdd75f73cdd190e5
Student <example@example.com>
2019-10-24 11:57:40 -0700

    create html for animal wiki webpage.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+
+$(document).ready(function() {
+  // var tahr = $("#tahr");
+  // var panda = $("#panda");
+  // var leopard = $("#leopard");
+
+  if ("#tahr").click(function() {
+    var tahr = $("#tahr");
+    $("#tahr-text").show();
+  });
+});
